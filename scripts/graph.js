9ec9c93ef47e0a01fd4d48e6d2bac3ec7f84291e
Izzy Brand <isaiah_brand@brown.edu>
2019-04-28 14:23:19 -0400

    pulled big graph out into its own module

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,3 +1,74 @@
+class Graph {
+    constructor(title, storeLast) {
+        this.storeLast = storeLast;
+        this.vals = new Deque(this.storeLast);
+
+        this.max = 1.0;
+        this.min = 1.0;
+
+        this.title = title;
+    }
+
+    addVal(val) {
+        this.max = Math.max(this.max, val);
+        this.min = Math.min(this.min, val);
+        this.vals.enqueue(val);
+        if (this.vals.length > this.storeLast) {
+            this.vals.dequeue();
+        }
+    }
+
+    plot() {
+        let vals = this.vals.toArray();
+
+        const plotScale = 0.6;
+        const topLeftX = w*(1-plotScale)/2;
+        const topLeftY = h*(1-plotScale)/2;
+        const plotWidth = w * plotScale;
+        const plotHeight = h * plotScale;
+        const border = plotHeight * 0.1;
+
+        const textInset = 5;
+        const numTicks = 5;
+        
+        // draw the background  of the plot
+        fill(90, 90, 90, 100);
+        rect(topLeftX, topLeftY, plotWidth, plotHeight, 5);
+
+        // draw the title and the scale along the y axis
+        fill(200);
+        textAlign(CENTER, TOP);
+        text(this.title, topLeftX+plotWidth/2, topLeftY+textInset);
+        textAlign(LEFT, TOP);
+        for (var i=0; i <= numTicks; i++) {
+            let xPos = topLeftX + textInset;
+            let yPos = topLeftY + border + textInset + i*(plotHeight-textInset*2 - textSize() - 2*border)/numTicks;
+            let val = this.max - i*(this.max - this.min)/numTicks
+            text(val.toPrecision(4), xPos, yPos);
+        }
+
+        // helpers to get x and y plot coordinates for each data point
+        let getxc = i => topLeftX + border + (plotWidth - 2*border) * ( i /vals.length);
+        let getyc = o => map(o, this.min, this.max, topLeftY+plotHeight - border, topLeftY + border);
+
+        stroke(212, 175, 55);
+        var xcp = getxc(0);
+        var ycp = getyc(vals[0]);
+        for (let i = 1; i < vals.length; i++) {
+            let occlusion = vals[i];
+            let xc = getxc(i);
+            let yc = getyc(occlusion);
+            if (!isNaN(yc)) {
+                line(xc, yc, xcp, ycp);
+                ycp = yc;
+                xcp = xc;
+            }
+        }
+    }
+}
+
+
+
 class OcclusionGraph {
     constructor(star, model) {
         this.storeLast = 256;
@@ -11,6 +82,8 @@ class OcclusionGraph {
         
         this.height = 30;
         this.width = this.storeLast;
+
+        this.bigGraph = new Graph(`${this.star.name} occlusion`, this.storeLast);
     }
 
     update() {
@@ -23,6 +96,8 @@ class OcclusionGraph {
         if (this.occlusions.length > this.storeLast) {
             this.occlusions.dequeue();
         }
+
+        this.bigGraph.addVal(totalOcclusion);
     }
 
     miniplot(index) {
@@ -45,52 +120,6 @@ class OcclusionGraph {
     }
 
     bigplot(index) {
-        let occlusions = this.occlusions.toArray();
-
-        const plotScale = 0.6;
-        const topLeftX = w*(1-plotScale)/2;
-        const topLeftY = h*(1-plotScale)/2;
-        const plotWidth = w * plotScale;
-        const plotHeight = h * plotScale;
-        const border = plotHeight * 0.1;
-
-        const textInset = 5;
-        const numTicks = 5;
-        
-        // draw the background  of the plot
-        fill(90, 90, 90, 100);
-        rect(topLeftX, topLeftY, plotWidth, plotHeight, 5);
-
-        // draw the title and the scale along the y axis
-        fill(200);
-        textAlign(CENTER, TOP);
-        text(`${this.star.name} occlusion`, topLeftX+plotWidth/2, topLeftY+textInset);
-        textAlign(LEFT, TOP);
-        for (var i=0; i <= numTicks; i++) {
-            let xPos = topLeftX + textInset;
-            let yPos = topLeftY + border + textInset + i*(plotHeight-textInset*2 - textSize() - 2*border)/numTicks;
-            let val = this.max - i*(this.max - this.min)/numTicks
-            text(val.toPrecision(4), xPos, yPos);
-        }
-
-        // helpers to get x and y plot coordinates for each data point
-        let getxc = i => topLeftX + border + (plotWidth - 2*border) * ( i /occlusions.length);
-        let getyc = o => map(o, this.min, this.max, topLeftY+plotHeight - border, topLeftY + border);
-
-
-
-        stroke(212, 175, 55);
-        var xcp = getxc(0);
-        var ycp = getyc(occlusions[0]);
-        for (let i = 1; i < occlusions.length; i++) {
-            let occlusion = occlusions[i];
-            let xc = getxc(i);
-            let yc = getyc(occlusion);
-            if (!isNaN(yc)) {
-                line(xc, yc, xcp, ycp);
-                ycp = yc;
-                xcp = xc;
-            }
-        }
+        this.bigGraph.plot();
     }
 }
